cdaf613f12ab54491d8b9f0cf7931cad5d7c29c8
Olivier Santerre <olisant@bitbucket.org>
2020-03-04 14:55:43 -0500

    fix rognage

--- src/application.cpp
+++ src/application.cpp
@@ -66,22 +66,26 @@ void Application::onButtonEvent(ofxDatGuiButtonEvent event)
   }
   if(event.target == boutonRogner)
   {
-        float width = renderer.croping_zone[2] - renderer.croping_zone[0];
-        float height = renderer.croping_zone[3] - renderer.croping_zone[1];
-        ImageStruct* image = &(renderer.images[renderer.images.size() - 1]);
-        int image_width = (int)(width / image->image_width * image->image.getWidth());
-        int image_heigth = (int)(height / image->image_heigth * image->image.getHeight());
-        int pixel_origin_x = (int)((float)renderer.croping_zone[0]/image->image_width * image->image.getWidth()) - (int)((float)image->image_origin_x/image->image_width * image->image.getWidth());
-        int pixel_origin_y = (int)((float)renderer.croping_zone[1]/image->image_heigth * image->image.getHeight()) - (int)((float)image->image_origin_y/image->image_heigth * image->image.getHeight());
-        image->image.cropFrom(image->image,
-                                pixel_origin_x,
-                                pixel_origin_y,
-                                image_width,
-                                image_heigth);
-        image->image_origin_x = renderer.croping_zone[0];
-        image->image_origin_y = renderer.croping_zone[1];
-        image->image_width = width;
-        image->image_heigth = height;
+        if (!renderer.croping_zone.empty())
+        {
+          float width = renderer.croping_zone[2] - renderer.croping_zone[0];
+          float height = renderer.croping_zone[3] - renderer.croping_zone[1];
+          ImageStruct* image = &(renderer.images[renderer.images.size() - 1]);
+          int image_width = (int)(width / image->image_width * image->image.getWidth());
+          int image_heigth = (int)(height / image->image_heigth * image->image.getHeight());
+          int pixel_origin_x = (int)((float)renderer.croping_zone[0]/image->image_width * image->image.getWidth()) - (int)((float)image->image_origin_x/image->image_width * image->image.getWidth());
+          int pixel_origin_y = (int)((float)renderer.croping_zone[1]/image->image_heigth * image->image.getHeight()) - (int)((float)image->image_origin_y/image->image_heigth * image->image.getHeight());
+          image->image.cropFrom(image->image,
+                                  pixel_origin_x,
+                                  pixel_origin_y,
+                                  image_width,
+                                  image_heigth);
+          image->image_origin_x = renderer.croping_zone[0];
+          image->image_origin_y = renderer.croping_zone[1];
+          image->image_width = width;
+          image->image_heigth = height;
+        }
+
   }
 
 }
